59f3089b15453f1c208b22ced89133812f023ec6
laksh mishra <lakshmishraofficial@gmail.com>
2024-12-27 21:32:09 +0530

    added back for future usage if needed

--- packages/hms_room_kit/lib/src/meeting/meeting_navigation_visibility_controller.dart
+++ packages/hms_room_kit/lib/src/meeting/meeting_navigation_visibility_controller.dart
@@ -9,5 +9,30 @@ import 'package:flutter/widgets.dart';
 class MeetingNavigationVisibilityController extends ChangeNotifier {
   bool showControls = true;
 
-  void toggleControlsVisibility() {}
+  ///This variable stores whether the timer is active or not
+  ///
+  ///This is done to avoid multiple timers running at the same time
+  bool _isTimerActive = false;
+
+  ///This method toggles the visibility of the buttons
+  void toggleControlsVisibility() {
+    showControls = true;
+
+    ///If the controls are now visible and
+    ///If the timer is not active, we start the timer
+    if (showControls && !_isTimerActive) {
+      startTimerToHideButtons();
+    }
+    notifyListeners();
+  }
+
+  ///This method starts a timer for 5 seconds and then hides the buttons
+  void startTimerToHideButtons() {
+    _isTimerActive = true;
+    Timer(const Duration(seconds: 5), () {
+      showControls = true;
+      _isTimerActive = false;
+      notifyListeners();
+    });
+  }
 }
